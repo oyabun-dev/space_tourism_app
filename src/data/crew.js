6f0ce9a44826dc9cc4553bc2ab622904360ee06a
oyabun-dev <khalifaababacarbeye@esp.sn>
2024-04-08 01:57:28 +0000

    Add new styles and components for Crew page

---
+++ src/data/crew.js
@@ -0,0 +1,35 @@
+import douglasHurley from '../assets/img/crew/image-douglas-hurley.png';
+import markShuttleworth from '../assets/img/crew/image-mark-shuttleworth.png';
+import victorGlover from '../assets/img/crew/image-victor-glover.png';
+import anoushehAnsari from '../assets/img/crew/image-anousheh-ansari.png';
+
+export const crewMembers = [
+    {
+        id: 1,
+        name: 'Douglas Hurley',
+        image: douglasHurley,
+        role: 'Commander',
+        description: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.'
+    },
+    {
+        id: 2,
+        name: 'Mark Shuttleworth',
+        image: markShuttleworth,
+        role: 'Mission Specialist',
+        description: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
+    },
+    {
+        id: 3,
+        name: 'Victor Glover',
+        image: victorGlover,
+        role: 'Pilot',
+        description: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.'
+    },
+    {
+        id: 4,
+        name: 'Anousheh Ansari',
+        image: anoushehAnsari,
+        role: 'Mission Specialist',
+        description: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.'
+    }
+];
--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
+import PageTitle from '../components/PageTitle';
+import { crewMembers } from '../data/crew';
+
+const pageTitle = {
+    id: '02',
+    title: 'Meet your Crew'
+};
 
 export default function Crew() {
+    const [crew, setCrew] = useState(crewMembers[0]);
+    const updateCrew = (e) => {
+        const id = parseInt(e.target.dataset.id);
+        setCrew(crewMembers[id]);
+        const linksNode = document.querySelectorAll('.crew-link');
+        linksNode.forEach(link => {
+            linksNode.forEach(item => item.classList.remove('active'));
+            e.target.classList.add('active');
+        });
+    }
     return (
-        <>
+        <div id='crew'>
             <Navbar />
-            <div>Crew</div>
-        </>
+            <div className='crew'>
+                <PageTitle id={pageTitle.id} title={pageTitle.title} />
+                <div className='crew-content'>
+                    <div className='crew-member'>
+                        <div className='crew-member-detail'>
+                            <h4 className='h4'>{crew.role}</h4>
+                            <h3 className='h3'>{crew.name}</h3>
+                            <p className='body-text'>{crew.description}</p>
+                        </div>
+                        <nav className='crew-nav'>
+                            <li className='crew-link active' data-id='0' onClick={updateCrew}></li>
+                            <li className='crew-link' data-id='1' onClick={updateCrew}></li>
+                            <li className='crew-link' data-id='2' onClick={updateCrew}></li>
+                            <li className='crew-link' data-id='3' onClick={updateCrew}></li>
+                        </nav>
+                    </div>
+                    <img src={crew.image} />
+                </div>
+            </div>
+        </div>
     )
 }
